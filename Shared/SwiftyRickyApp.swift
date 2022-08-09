3eb32d6c8dc688a9e1cc99c393b796c4cd7aaf88
Eduardo Shinkawa <eshinkawa@gmail.com>
2022-08-09 19:07:26 -0300

    Initial Commit

---
+++ Shared/SwiftyRickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyRickyApp.swift
+//  Shared
+//
+//  Created by Eduardo Shinkawa on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyRickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
